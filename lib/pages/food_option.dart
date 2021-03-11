b8dc3a6f612f1675cd8fb2fc3b3d2ba8ac4e5e7e
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-03-11 19:21:05 +0700

    place other successfully : 3/3

--- lib/pages/food_option.dart
+++ lib/pages/food_option.dart
@@ -18,7 +18,7 @@ class _FoodOptionState extends State<FoodOption> {
           children: <Widget>[
             ElevatedButton(
               child: Text('Show Cupertino Dialog'),
-              onPressed: _showCupertinoDialog,
+              onPressed: _showAlertDialog,
             ),
           ],
         ),
@@ -85,6 +85,93 @@ class _FoodOptionState extends State<FoodOption> {
             },
           )
         ],
+      ),
+    );
+  }
+
+  _showAlertDialog() {
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16.0),
+          ),
+        ),
+        title: Container(
+          width: 243,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                // padding: EdgeInsets.only(top: 16),
+                width: 65,
+                height: 65,
+                child: Image.asset('assets/images/1.0x/check_circle.png'),
+              ),
+              _buidlSizedBox(4),
+              Text(
+                "You Place the Order Successfully",
+                style:
+                    StylesText.bodyText2.copyWith(fontWeight: FontWeight.bold),
+              ),
+              // _buidlSizedBox(2),
+            ],
+          ),
+        ),
+        content: Container(
+          height: 100,
+          width: 303,
+          child: Column(
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(children: [
+                  TextSpan(
+                    text:
+                        "You placed the order successfully. You will get your food within ",
+                    style: StylesText.caption.copyWith(
+                      color: AppColors.neutral1,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "15 minutes. ",
+                    style: StylesText.caption.copyWith(
+                      color: AppColors.neutral1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Thanks for using our services. Enjoy your food!",
+                    style: StylesText.caption.copyWith(
+                      color: AppColors.neutral1,
+                    ),
+                  ),
+                ]),
+              ),
+              _buidlSizedBox(4),
+              Container(
+                height: 40,
+                width: 210,
+                child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(primary: AppColors.neutralWhite),
+                  child: Text(
+                    'Keep Browsing',
+                    style: StylesText.bodyText1.copyWith(
+                      color: AppColors.primaryOrangeRed,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
